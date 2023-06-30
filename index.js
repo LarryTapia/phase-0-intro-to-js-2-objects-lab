f5c2cb3c9b2166e0ee7a87e69a45b7866fabd114
Larry Tapia <tapialarry1@gmail.com>
2023-06-30 01:21:12 -0400

    completed lab

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
 // Write your solution in this file!
+
+ const employee = {
+    name : `Larry`,
+    streetAddress : `295 Fort Washington Ave`
+ };
+
+ function updateEmployeeWithKeyAndValue(employee, key, value){
+    return {...employee,[key]: value,};
+ }
+
+ function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value){
+    employee[key] = value;
+    return employee;
+ }
+
+ function deleteFromEmployeeByKey(employee, key){
+    const copyEmployee = {...employee};
+    delete copyEmployee[key];
+    return copyEmployee;
+ }
+
+ function destructivelyDeleteFromEmployeeByKey(employee, key){
+    delete employee[key];
+    return employee;
+ }
